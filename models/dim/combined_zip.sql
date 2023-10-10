43b89c18a27564827d0974523e4f8aa0d62ae9f8
Jeremy Moreland <9988053+jmorelandsbiz@users.noreply.github.com>
2023-10-10 12:38:09 -0500

    no Packers

--- models/dim/combined_zip.sql
+++ models/dim/combined_zip.sql
@@ -3,6 +3,3 @@ from {{ ref('mi_zip') }}
 union all
 select top 20 zip_id, zip, city, state, sourcesystem
 from {{ ref('mn_zip')}}
-union all
-select top 31 zip_id, zip, city, state, sourcesystem
-from {{ ref('wi_zip')}}
