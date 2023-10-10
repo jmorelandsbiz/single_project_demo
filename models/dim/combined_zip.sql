e51c67db7dbb7d11169da4551651b22d6c70d4f3
Jeremy Moreland <9988053+jmorelandsbiz@users.noreply.github.com>
2023-10-10 12:25:11 -0500

    31 for wi

--- models/dim/combined_zip.sql
+++ models/dim/combined_zip.sql
@@ -4,5 +4,5 @@ union all
 select top 20 zip_id, zip, city, state, sourcesystem
 from {{ ref('mn_zip')}}
 union all
-select top 20 zip_id, zip, city, state, sourcesystem
+select top 31 zip_id, zip, city, state, sourcesystem
 from {{ ref('wi_zip')}}
