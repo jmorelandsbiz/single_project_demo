11a957e4e9591570bbe656e37239f5ef6d02d0c2
Jeremy Moreland <9988053+jmorelandsbiz@users.noreply.github.com>
2023-10-10 13:17:10 -0500

    Added wisconsin

--- models/dim/combined_zip.sql
+++ models/dim/combined_zip.sql
@@ -3,3 +3,6 @@ from {{ ref('mi_zip') }}
 union all
 select top 20 zip_id, zip, city, state, sourcesystem
 from {{ ref('mn_zip')}}
+union all
+select top 20 zip_id, zip, city, state, sourcesystem
+from {{ ref('wi_zip')}}
